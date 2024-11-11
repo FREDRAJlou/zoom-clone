73b4b9b2a5decee9e9112a711d9c5a8f9032a43a
fa <fa@ford.com>
2024-11-11 16:03:22 +0530

    Sidebar fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import StreamVideoProvider from '@/providers/StreamClientProvider'
 import { Metadata } from 'next';
-import React, { ReactNode } from 'react'
+import React from 'react'
 
 export const metadata: Metadata = {
   title: "Zoom Clone",
